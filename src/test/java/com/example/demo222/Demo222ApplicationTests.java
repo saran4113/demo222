f0131c6c3de11cd6134a184fa50ad3e97906273b
saran <saran4113@thekary.com>
2021-10-12 17:57:56 +0900

    Initial commit66

---
+++ src/test/java/com/example/demo222/Demo222ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo222;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demo222ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
